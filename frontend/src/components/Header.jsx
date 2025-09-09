06aca8407769c865107baa4adc85fd348117e92d
mshahdevs <mshahaurangzeb45@gmail.com>
2025-09-09 17:30:27 +0500

    added header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+const Header = () => {
+  return (
+    <header>
+      <Navbar expand='lg' className='bg-dark' variant='dark' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              {/* <img src='../logo.svg' width={100} alt='' /> */}
+              ShoPay
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  {" "}
+                  <i className='fas fa-shopping-cart' /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user' />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
