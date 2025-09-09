944ea0ac6a5e583aca08ffda760fd4b661a3f5eb
mshahdevs <mshahaurangzeb45@gmail.com>
2025-09-09 17:28:54 +0500

    setup router v6 for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./components/screens/HomeScreen";
+import ProductScreen from "./components/screens/ProductScreen";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>App Page</h1>
+      <Router>
+        <Header />
+
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} />
+              <Route path='/product/:id' element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 }
