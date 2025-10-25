6c2f17d690bbead2688efc68d6ad1603fb11438b
mshahdevs <mshahaurangzeb45@gmail.com>
2025-10-25 15:59:58 +0500

    add user controller with authentication and registration functions

---
+++ backend/controller/userController.js
@@ -0,0 +1,78 @@
+const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
+// @desc    Get user profile
+// @route   GET /api/users/login
+// @access  Public
+
+const authUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const userRegister = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ message: "User already exists" });
+      return;
+    }
+    const user = await User.create({ name, email, password });
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const getUserProfile = async (req, res) => {
+  try {
+    if (!req.user) {
+      res
+        .status(401)
+        .json({ message: "Not authorized, no user attached to request" });
+      return;
+    }
+
+    const user = await User.findById(req.user._id);
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error("getUserProfile error:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+module.exports = { authUser, getUserProfile, userRegister };
