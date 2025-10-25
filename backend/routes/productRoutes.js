160e9ae46c846b5d7ac71093e1f1a1c49f5a6ec6
mshahdevs <mshahaurangzeb45@gmail.com>
2025-10-25 12:03:36 +0500

    refactor product routes to use controller functions for fetching products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,20 +1,10 @@
 const express = require("express");
-// const products = require("../data/products");
 const router = express.Router();
-const Product = require("../models/productModel");
-router.get("/", async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-});
-
-router.get("/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404);
-    throw new Error("Product not found");
-  }
-});
+const {
+  getProducts,
+  getProductById,
+} = require("../controller/productController");
+router.get("/", getProducts);
+router.get("/:id", getProductById);
 
 module.exports = router;
