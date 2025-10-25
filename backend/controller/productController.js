6818c6eed0d91f3689dd262ddf3f650839aee5bc
mshahdevs <mshahaurangzeb45@gmail.com>
2025-10-25 12:03:25 +0500

    add product controller with routes for fetching all products and product by ID

---
+++ backend/controller/productController.js
@@ -0,0 +1,32 @@
+const Product = require("../models/productModel.js");
+
+// @desc    Get all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc    Get product by ID
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getProducts, getProductById };
