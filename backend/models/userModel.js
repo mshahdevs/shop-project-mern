c67e408bbe8895c598d0b474ce2b96fed56e12e0
mshahdevs <mshahaurangzeb45@gmail.com>
2025-10-25 16:00:17 +0500

    fix: correct bcrypt import and adjust matchPassword method formatting

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -25,7 +25,9 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
-
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
