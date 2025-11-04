2bc259298642b32a3ef6998a4e71ec400a10cebb
mshahdevs <mshahaurangzeb45@gmail.com>
2025-11-04 17:19:51 +0500

    refactor user registration error handling for existing users

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -30,8 +30,8 @@ const userRegister = async (req, res) => {
     const { name, email, password } = req.body;
     const userExists = await User.findOne({ email });
     if (userExists) {
-      res.status(400).json({ message: "User already exists" });
-      return;
+      res.status(400);
+      throw new Error("User already exists");
     }
     const user = await User.create({ name, email, password });
     if (user) {
