3196cc7bfd3571c9bbb370c75c4bc5d8d9325d44
mshahdevs <mshahaurangzeb45@gmail.com>
2026-02-13 08:19:58 +0500

    add OrderScreen component for displaying order details and summary

---
+++ frontend/src/components/screens/OrderScreen.jsx
@@ -0,0 +1,123 @@
+import { Link ,useNavigate, useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import { Button,  Row,Col, ListGroup,Image, Card } from "react-bootstrap";
+import Loader from "../Loader";
+import Message from "../Message";
+import {getOrderDetails} from "../../actions/orderActions";
+import { useEffect } from "react";
+const OrderScreen = () => {
+    const orderDetails = useSelector(state => state.orderDetails);
+    const {loading,error,order} = orderDetails;
+    const {id} = useParams();
+     function addDecimals(num){
+        return (Math.round(num * 100) / 100).toFixed(2);
+    }
+    order.itemsPrice = addDecimals(order.orderItems.reduce((acc,item)=> acc + item.price * item.qty,0).toFixed(2));
+   
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getOrderDetails(id));
+    },[dispatch,id])
+    return loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : <>
+    <h1>Order {order._id}</h1>
+    <Row>
+    
+        <Col md={8} >
+        <ListGroup variant="flush">
+            <ListGroup.Item>
+                <h2>Shipping</h2>
+                <p><strong>Name:</strong> {order.user.name}</p>
+                    <p><strong>Email:</strong> <a href={`mailto:${order.user.email}`}>{order.user.email}</a></p>
+                <p>
+                    <strong>Address:</strong>
+                    {" "}{order.shippingAddress.address}{" "},{order.shippingAddress.city}{" "}{order.shippingAddress.postalCode},{order.shippingAddress.country}
+                </p>
+                {order.isDelivered ? <Message variant="success">Delivered on {order.deliveredAt}</Message> : <Message variant="danger">Not Delivered</Message>}
+            </ListGroup.Item>
+            <ListGroup.Item>
+                <h1>Payment Method</h1>
+                <p>
+
+                <strong>Method:</strong>
+                {" "}{order.paymentMethod}
+                </p>
+                {order.isPaid ? <Message variant="success">Paid on {order.paidAt}</Message> : <Message variant="danger">Not Paid</Message>}
+            </ListGroup.Item>
+            <ListGroup.Item>
+                <h1>Order Items</h1>
+                {order.orderItems.length === 0 ?  <Message>Your order is empty.</Message> : (
+                    <ListGroup variant="flush">
+                        {order.orderItems.map((item,index)=>(
+                            <ListGroup.Item key={index}>
+                                  <Row>
+                                    <Col md={1}>
+                                    <Image src={item.image} alt={item.name} 
+                                    fluid rounded />
+                                    </Col>
+                                    <Col>
+                                    <Link to={`/product/${item.product}`}>
+                                      {item.name}
+                                    </Link>
+                                    </Col>
+                                     <Col>
+                                    
+                                      {item.qty} x ${item.price} = ${(item.qty * item.price).toFixed(2)}
+                                
+                                    </Col>
+                                    
+                                  </Row>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                )}
+            </ListGroup.Item>
+        </ListGroup>
+        </Col>
+        <Col md={4}>
+                                      <Card>
+                                        <ListGroup variant="flush">
+                                             <ListGroup.Item>
+                                                <h2>Order Summary</h2>
+                                             </ListGroup.Item>
+                                             <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Items
+                                                    </Col>
+                                                    <Col>${order.itemsPrice}</Col>
+                                                </Row>
+                                                
+                                             </ListGroup.Item>
+                                              <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Shipping
+                                                    </Col>
+                                                    <Col>${order.shippingPrice}</Col>
+                                                </Row>
+                                                
+                                             </ListGroup.Item>
+                                              <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Tax
+                                                    </Col>
+                                                    <Col>${order.taxPrice}</Col>
+                                                </Row>
+                                                
+                                             </ListGroup.Item>
+                                              <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Total
+                                                    </Col>
+                                                    <Col>${order.totalPrice}</Col>
+                                                </Row>
+                                                
+                                             </ListGroup.Item>
+                                             
+                                        </ListGroup>
+                                      </Card>
+                                    </Col>
+        </Row>
+    </>
+}
+
+export default OrderScreen;
