afaa9d8d7a8b08fc00ef3e93c5355069ba334c43
mshahdevs <mshahaurangzeb45@gmail.com>
2026-05-14 00:06:48 +0500

    feat: add USER_DETAILS_RESET case to userDetailsReducer and improve formatting

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -12,7 +12,8 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
-} from "../constants/userConstant";
+  USER_DETAILS_RESET,
+} from '../constants/userConstant';
 
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
@@ -42,28 +43,29 @@ export const userRegisterReducer = (state = {}, action) => {
   }
 };
 
-export const userDetailsReducer = (state={user:{}},action) =>{
-  switch(action.type){
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
     case USER_DETAILS_REQUEST:
-      return {...state,loading:true};
+      return { ...state, loading: true };
     case USER_DETAILS_SUCCESS:
-      return {loading:false,user:action.payload};
+      return { loading: false, user: action.payload };
     case USER_DETAILS_FAIL:
-      return {loading:false,error:action.payload};
+      return { loading: false, error: action.payload };
+    case USER_DETAILS_RESET:
+      return { user: {} };
     default:
       return state;
   }
-}
-export const userUpdateReducer = (state={},action) =>{
-  switch(action.type){
+};
+export const userUpdateReducer = (state = {}, action) => {
+  switch (action.type) {
     case USER_UPDATE_PROFILE_REQUEST:
-      return {loading:true};
+      return { loading: true };
     case USER_UPDATE_PROFILE_SUCCESS:
-      return {loading:false,success:true,userInfo:action.payload};
+      return { loading: false, success: true, userInfo: action.payload };
     case USER_UPDATE_PROFILE_FAIL:
-      return {loading:false,error:action.payload};
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
-}
-
+};
