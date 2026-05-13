3cfef407580243cda365a16598299e86b051d128
mshahdevs <mshahaurangzeb45@gmail.com>
2026-05-14 00:05:31 +0500

    feat: add route to retrieve user's orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const router = express.Router();    
 const {protect} =  require("../middleware/authMiddleware");
-const { addOrderItems, getOrderById, updateOrderToPaid } =require( "../controller/orderController");
+const { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders } =require( "../controller/orderController");
 router.post("/", protect, addOrderItems);
+router.get("/myorders", protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 module.exports = router;
