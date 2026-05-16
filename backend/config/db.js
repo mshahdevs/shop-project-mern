95bab761757d09898acdf903520e7b797052d0d1
mshahdevs <mshahaurangzeb45@gmail.com>
2026-05-16 14:35:31 +0500

    fix: standardize quotation marks and update MongoDB URI variable in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
       connectTimeoutMS: 10000,
