0054ed4c75e400f9ef5fbd7f09c74709cae4e706
mshahdevs <mshahaurangzeb45@gmail.com>
2026-05-17 17:43:08 +0500

    fix: standardize quotation marks in user data for consistency

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,19 +1,20 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 const users = [
   {
-    name: "Admin User",
-    email: "admin@example.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
   },
   {
-    name: "John Doe",
-    email: "john@example.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
   },
   {
-    name: "Jane Deniel",
-    email: "jane@example.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'Jane Deniel',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
   },
 ];
 
